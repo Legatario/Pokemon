d3cb476a041f11bc8dfc4c641b6b10e57419063e
Luis Phillipe <l.phillipe.d@gmail.com>
2022-07-07 15:23:01 -0300

    colocando comentarios

--- src/app/componets/pokemon-card/pokemon-card.component.ts
+++ src/app/componets/pokemon-card/pokemon-card.component.ts
@@ -18,6 +18,8 @@ export class PokemonCardComponent {
     return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${numberPokemon}.png`;
   }
 
+  // retornar 1 ou dois zero para integrar na imagem assim mostrando o pokemon com o memos numero de seu ID, sem estes 000 ele não puxa a imagem
+
   leadingZero(str: string | number, size = 3) : string {
     let s = String(str);
 
